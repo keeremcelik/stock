6f0f2147d289de43c09861bfb1d1316f9d9e55ae
arslan demir <isosaslan@gmail.com>
2020-03-14 21:17:51 +0300

    devs

--- app/Http/Middleware/kullanici.php
+++ app/Http/Middleware/kullanici.php
@@ -17,16 +17,11 @@ class kullanici
     {   
         $kullanici = $request->session()->get('kullanici');
         
-<<<<<<< HEAD
         if (isset($kullanici->id)) {
             echo "<pre>";
                 print_r($kullanici->id);
                 print_r($kullanici->name." ".$kullanici->surname);
             echo "</pre>";
-=======
-        if ($kullanici->id) {
-           
->>>>>>> d6b7fd31b40643edec00a58b51339b52b98af0f2
         }else{
             return redirect()->back()->with('message','hataaa');
             die;
